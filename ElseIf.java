9b492bef9b6f03138257796baeda06aef18c7bb5
KevKevTheWebDev <student.122454@worc.k12.ma.us>
2015-07-21 17:13:22 -0400

    Update and rename README.md to ElseIf.java

---
+++ ElseIf.java
@@ -0,0 +1,13 @@
+class apples{
+	public static void main(String args[]){
+	int age = 49;
+	if (age>60)
+	System.out.println("You're older than 60");
+	else if (age>=50)
+		System.out.println("You are in your 50s");
+	else if (age>=40)
+		System.out.println("You are in your 40s");
+	else
+		System.out.println("You're younger than 60");
+	}
+}
